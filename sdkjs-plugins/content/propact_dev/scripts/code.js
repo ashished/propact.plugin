edd551f2ecf3ad2a36632974d9e4f206b892673d
Ashish <ashish.encodedots@gmail.com>
2023-07-25 11:44:27 +0530

    Update code for get all content controls

--- sdkjs-plugins/content/propact_dev/scripts/code.js
+++ sdkjs-plugins/content/propact_dev/scripts/code.js
@@ -25,6 +25,19 @@
 
     window.Asc.plugin.init = function (text) {
 
+        if (!flagInit) {
+            this.executeMethod ("GetAllContentControls", null, function (data) {
+                for (var i = 0; i < data.length; i++) {
+                    console.log('ContentControls ' + i, data[i]);
+                    // if (data[i].Tag == 11) {
+                        // this.Asc.plugin.executeMethod ("SelectContentControl", [data[i].InternalId]);
+                        // break;
+                    // }
+                }
+            });
+            flagInit = true;
+        }
+
         var sDocumentEditingRestrictions = "readOnly";
         window.Asc.plugin.executeMethod("SetEditingRestrictions", [sDocumentEditingRestrictions]);
 
