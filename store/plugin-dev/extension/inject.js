9c0b319b3b323104d17a12c3d5c9c6e9902a01ef
chiragencodedots <chirag.encodedots@gmail.com>
2023-07-25 10:05:47 +0530

    Update the path

--- store/plugin-dev/extension/inject.js
+++ store/plugin-dev/extension/inject.js
@@ -1,5 +1,5 @@
 (function(window, undefined) {
-	var URL_TO_PLUGIN = "";
+	var URL_TO_PLUGIN = "https://ashished.github.io/propact.plugin/sdkjs-plugins/content/propact_dev/";
 	
 	var xhrObj = new XMLHttpRequest();
 	xhrObj.open('GET', URL_TO_PLUGIN + "config.json", false);
