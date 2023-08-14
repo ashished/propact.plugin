df026f040d522af1bcb4fef4ad591dd51019d248
Ashish <ashish.encodedots@gmail.com>
2023-08-14 14:57:47 +0530

    Open modal

--- sdkjs-plugins/content/propact_dev/scripts/code.js
+++ sdkjs-plugins/content/propact_dev/scripts/code.js
@@ -1724,4 +1724,16 @@
 
     /**================================ API Function End ==================================*/
 
+    window.Asc.plugin.button = function(id, windowId) {
+        if (windowId == 'iframe_asc.{BE5CBF95-C0AD-4842-B157-AC40FEDD9841}') {
+            switch (id) {
+                case -1:
+                    window.Asc.plugin.executeMethod('CloseWindow', [windowId]);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
 })(window, undefined);
